6eddaafcf12864d3cf65ed12fc02ef98f84a4601
tongb <tongb@DESKTOP-MILDV4C>
2021-08-25 15:51:53 +0700

    stateful

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My app",
-      home: Scaffold(
+      home: MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.pink),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
-          title: Text("Flutter"),
+          title: Text("Counter"),
         ),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[Text("01"), Text("02"), Text("03"),Image(image: NetworkImage("http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcS-lrRUd1IHPGjT4Z7PZJh3BfB4deUXE_A95Syki1EFR1KHOnUrau0pSMD3SOuu"))],
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[Text("01"), Text("02"), Text("0sdsdsd"),Image(image: NetworkImage("http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcS-lrRUd1IHPGjT4Z7PZJh3BfB4deUXE_A95Syki1EFR1KHOnUrau0pSMD3SOuu"))],
           ),
         ),
-      ),
-      theme: ThemeData(primarySwatch: Colors.pink),
-    );
+      );
   }
 }
