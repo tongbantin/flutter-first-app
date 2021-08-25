99f23fc9e085c3aee71d1716dd65c223bb5eb63b
tongb <tongb@DESKTOP-MILDV4C>
2021-08-25 15:34:13 +0700

    text

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = MaterialApp(
-    title: "My app",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter"),
-      ),
-      body: Text("Hello flutter"),
-    ),
-  );
+  var app = MyApp();
   runApp(app);
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My app",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter"),
+        ),
+        body: Center(
+          child: Text(
+            "Hello flutterss sdass",
+            style: TextStyle(fontSize: 30, color: Colors.purple),
+          ),
+        ),
+      ),
+      theme: ThemeData(primarySwatch: Colors.pink),
+    );
+  }
 }
