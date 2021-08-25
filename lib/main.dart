f28aac3499bee7368e91f84a772774f26fd6048e
tongb <tongb@DESKTOP-MILDV4C>
2021-08-25 15:59:02 +0700

    button

--- lib/main.dart
+++ lib/main.dart
@@ -15,24 +15,41 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int number = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Counter"),
+      appBar: AppBar(
+        title: Text("Counter"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Numbre"),
+            Text(
+              number.toString(),
+              style: TextStyle(fontSize: 60),
+            ),
+            Text("0sdsdsd"),
+            Image(
+                image: NetworkImage(
+                    "http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcS-lrRUd1IHPGjT4Z7PZJh3BfB4deUXE_A95Syki1EFR1KHOnUrau0pSMD3SOuu"))
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[Text("01"), Text("02"), Text("0sdsdsd"),Image(image: NetworkImage("http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcS-lrRUd1IHPGjT4Z7PZJh3BfB4deUXE_A95Syki1EFR1KHOnUrau0pSMD3SOuu"))],
-          ),
-        ),
-      );
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        //child: Text('+',style: TextStyle(fontSize: 30),),
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
