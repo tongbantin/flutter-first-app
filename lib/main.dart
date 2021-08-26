eac43ccb1fff4537721574e22e7beeef251a26ce
tongb <tongb@DESKTOP-MILDV4C>
2021-08-26 16:40:56 +0700

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -24,37 +24,27 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int number = 0;
 
+//แสดงผล
   @override
   Widget build(BuildContext context) {
-    List<Widget> data = [];
-    data.add(Text("Send Love"));
-    data.add(Text(
-              number.toString(),
-              style: TextStyle(fontSize: 60),
-            ));
-    for (var i = 0; i < 10; i++) {
-      data.add(Text("Flutter :"+i.toString()));
-    }
     return Scaffold(
       appBar: AppBar(
         title: Text("PopLee"),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:data,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: addNumber,
-        child: Icon(Icons.add),
+        child: ListView(children: getData(50)),
       ),
     );
   }
 
-  void addNumber() {
-    setState(() {
-      number++;
-    });
+  List<Widget> getData(int count) {
+    List<Widget> data = [];
+    for (var i = 0; i < count; i++) {
+      data.add(Text(
+        "Flutter :" + i.toString(),
+        style: TextStyle(fontSize: 25),
+      ));
+    }
+    return data;
   }
 }
